d34df8761040bc0f1f164230a24f1bc17868e3f4
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-12-21 15:20:13 +0100

    chore: setup Elixir project
    Creates new elixir project via command `mix new <project name>`.

---
+++ lib/elixir_wordle.ex
@@ -0,0 +1,18 @@
+defmodule ElixirWordle do
+  @moduledoc """
+  Documentation for `ElixirWordle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirWordle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
