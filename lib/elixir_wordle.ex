1c5272fca6abc90825f0300de3211a420008c4c4
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-12-21 15:48:13 +0100

    feature: entrypoint via CLI
    Adds a `play` method to run the game. Uses the `Prompt` module to implement a simple CLI to interact with the game.

--- lib/elixir_wordle.ex
+++ lib/elixir_wordle.ex
@@ -1,18 +1,32 @@
 defmodule ElixirWordle do
   @moduledoc """
-  Documentation for `ElixirWordle`.
+   Play Wordle in your terminal - Implemented in Elixir
+
+   This method starts the game loop:
+
+   > mix run -e "ElixirWordle.main"
   """
 
-  @doc """
-  Hello world.
+  def play do
+    start_game? = Prompt.choice(
+      """
+        Welcome to
 
-  ## Examples
+        --- WORDLE ---
 
-      iex> ElixirWordle.hello()
-      :world
+        Do you want to play a new game?
+      """,
+      [yes: "y", no: "n"]
+    )
 
-  """
-  def hello do
-    :world
+    with :yes <- start_game? do
+      Prompt.display(
+        """
+          Time to play!
+        """
+      )
+    else
+      _ -> Prompt.display("See you next time!")
+    end
   end
 end
