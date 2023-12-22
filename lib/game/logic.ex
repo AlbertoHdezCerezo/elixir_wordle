ff961cba66d90f328b595be33aa4e40327b547b1
Alberto Hernandez Ceezo <pascu216@gmail.com>
2023-12-22 12:54:10 +0100

    chore: game logic test coverage

--- lib/game/logic.ex
+++ lib/game/logic.ex
@@ -3,32 +3,33 @@ defmodule Game.Logic do
 
   @doc "Updates game session with a new guess attempt"
   @spec resolve(Game.Session.t(), String.t()) :: Game.Session.t()
-  def resolve(%Game.Session{state: [:win | :lose]} = session), do: session
+  def resolve(%Game.Session{state: state} = session) when state in [:win, :lose], do: session
 
-  def resolve(%Game.Session{guesses: guesses, guesses_limit: guesses_limit, word: word_to_guess} = session, word)
+  def resolve(%Game.Session{guesses: guesses, guesses_limit: guesses_limit} = session, _)
     when length(guesses) >= guesses_limit, do: session
 
   def resolve(%Game.Session{guesses: guesses, guesses_limit: guesses_limit, word: word_to_guess} = session, word)
     when word_to_guess == word and length(guesses) < guesses_limit,
-      do: %{session | guesses: [guesses] ++ guess(session, word), state: :won}
+      do: %{session | guesses: guesses ++ [guess(session, word)], state: :win}
 
   def resolve(%Game.Session{guesses: guesses, guesses_limit: guesses_limit, word: word_to_guess} = session, word)
     when word_to_guess != word and length(guesses) == guesses_limit - 1,
-      do: %{session | guesses: [guesses] ++ guess(session, word), state: :lose}
+      do: %{session | guesses: guesses ++ [guess(session, word)], state: :lose}
 
   def resolve(%Game.Session{guesses: guesses} = session, word),
-    do: %{session | guesses: [guesses] ++ guess(session, word), state: :playing}
+    do: %{session | guesses: guesses ++ [guess(session, word)], state: :playing}
 
   @spec guess(Game.Session.t(), String.t()) :: Game.Session.guess()
   defp guess(%Game.Session{word: word_to_guess}, word) when word_to_guess == word,
-    do: word |> String.graphemes() |> Enum.map(fn char -> %{char: char, state: :correct} end)
+    do: word |> String.upcase |> String.graphemes |> Enum.map(fn char -> %{char: char, state: :correct} end)
 
-  defp guess(%Game.Session{}, word) do
-    correct_chars = word |> String.graphemes()
+  defp guess(session, word) do
+    correct_chars = session.word |> String.upcase |> String.graphemes
 
     word
-      |> String.graphemes()
-      |> Enum.with_index()
+      |> String.upcase
+      |> String.graphemes
+      |> Enum.with_index
       |> Enum.map(
         fn {char, index} ->
           state = case Enum.at(correct_chars, index) do
